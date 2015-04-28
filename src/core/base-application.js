9528479f7a9e44b9063dbb6c076ad4dcdd75fbee
danigar <daniel.garcia@bq.com>
2015-04-28 10:51:47 +0200

    Instance new view-controller object if the current instance exists and it's destroyed
    Change-Id: I8e27221243b207c26af5548e8ba0b3512fdaa8bc

--- src/core/base-application.js
+++ src/core/base-application.js
@@ -21,17 +21,19 @@ Jskeleton.BaseApplication = Marionette.Application.extend({
         Marionette.Application.prototype.constructor.apply(this, arguments);
     },
     //Call to the specified view-controller method for render a app state
-    invokeViewControllerRender: function(controllerView, args, handlerName) {
-        var hook = this.getHook();
-        this.triggerMethod('onNavigate', controllerView, hook);
+    invokeViewControllerRender: function(routeObject, args, handlerName) {
+        var hook = this.getHook(),
+            viewController = routeObject._viewController = this._getViewControllerInstance(routeObject);
+
+        this.triggerMethod('onNavigate', viewController, hook);
         hook.processBefore();
 
-        if (typeof controllerView[handlerName] !== 'function') {
+        if (typeof viewController[handlerName] !== 'function') {
             throw new Error('El metodo ' + handlerName + ' del view controller no existe');
         }
 
-        controllerView[handlerName].call(controllerView, args, this.service);
-        this._showControllerView(controllerView);
+        viewController[handlerName].call(viewController, args, this.service);
+        this._showControllerView(viewController);
         hook.processAfter();
     },
     _showControllerView: function(controllerView) {
@@ -57,41 +59,50 @@ Jskeleton.BaseApplication = Marionette.Application.extend({
         var self = this;
         this._viewControllers = [];
         if (this.routes) {
-            _.each(this.routes, function(routeOptions, routeName) {
-                routeOptions = routeOptions || {};
+            _.each(this.routes, function(routeObject, routeName) {
+                routeObject = routeObject || {};
                 //get view controller instance (it could be a view controller class asigned to the route or a default view controller if no class is specified)
-                var viewController = self._getViewController(routeOptions);
+                //get view controller extended class with d.i
+                routeObject._ViewController = self._extendViewControllerClass(routeObject);
+
+                routeObject._viewControllerOptions = _.extend({
+                    app: self,
+                    channel: self.privateChannel,
+                    service: self.service,
+                    region: self.region
+                }, routeObject.viewControllerOptions);
+
                 //add the route handler to Jskeleton.Router
-                self._addAppRoute(routeName, routeOptions, viewController);
+                self._addAppRoute(routeName, routeObject);
                 //add the event handler to the app globalChannel
-                self._addAppEventListener(routeName, routeOptions, viewController);
+                self._addAppEventListener(routeName, routeObject);
             });
         }
     },
     //
-    _addAppRoute: function(routeString, routeOptions, viewController) {
+    _addAppRoute: function(routeString, routeObject) {
         var self = this;
 
         this.router.route(routeString, {
-            triggerEvent: routeOptions.triggerEvent,
-            handlerName: routeOptions.handlerName || this._getViewControllerHandlerName(routeString)
+            triggerEvent: routeObject.triggerEvent,
+            handlerName: routeObject.handlerName || this._getViewControllerHandlerName(routeString)
         }, function(args, handlerName) {
-            self.invokeViewControllerRender(viewController, args, handlerName);
+            self.invokeViewControllerRender(routeObject, args, handlerName);
         });
     },
     //Add listen to a global event changing the url with the event parameters and calling to the view-controller
-    _addAppEventListener: function(routeString, routeOptions, viewController) {
-        if (routeOptions.eventListener) {
+    _addAppEventListener: function(routeString, routeObject) {
+        if (routeObject.eventListener) {
             var self = this,
-                handlerName = routeOptions.handlerName || this._getViewControllerHandlerName(routeString);
+                handlerName = routeObject.handlerName || this._getViewControllerHandlerName(routeString);
 
-            this.listenTo(this.globalChannel, routeOptions.eventListener, function(args) {
-                if (!routeOptions.navigate) {
+            this.listenTo(this.globalChannel, routeObject.eventListener, function(args) {
+                if (!routeObject.navigate) {
                     //update the url
-                    self._navigateTo.call(self, routeString, routeOptions, args);
+                    self._navigateTo.call(self, routeString, routeObject, args);
                 }
 
-                self.invokeViewControllerRender(viewController, args, handlerName);
+                self.invokeViewControllerRender(routeObject, args, handlerName);
             });
         }
     },
@@ -114,36 +125,41 @@ Jskeleton.BaseApplication = Marionette.Application.extend({
 
         return handlerName;
     },
-    //Get a view controller instance (if no view controller is specified, a default view controller class is instantiated)
-    _getViewController: function(options) {
-        var self = this,
-            template = options.template,
-            viewControllerOptions = _.extend({
-                app: this,
-                channel: this.privateChannel,
-                service: this.service,
-                region: this.region
-            }, options.viewControllerOptions),
-            ViewControllerClass = options.viewControllerClass || this.getDefaultviewController(),
-            viewController;
+    //Extend view controller class for inyect template dependency
+    _extendViewControllerClass: function(options) {
+
+        var template = options.template,
+            ViewControllerClass = options.viewControllerClass || this.getDefaultviewController();
 
         if (!template) {
             throw new Error('Tienes que definir un template');
         }
 
-        ViewControllerClass = this.extendViewController(ViewControllerClass, template);
-
-        viewController = this.factory(ViewControllerClass, viewControllerOptions);
-
-        this._viewControllers.push(viewController);
-
-        return viewController;
-    },
-    //Extend view controller class for inyect template dependency
-    extendViewController: function(ViewControllerClass, template) {
         return ViewControllerClass.extend({
             template: template
         });
+
+    },
+    _removeViewController: function(routeObject, viewController) {
+        if (routeObject._viewController === viewController) {
+            routeObject._viewController = undefined;
+        }
+    },
+    //Get a view controller instance (if no view controller is specified, a default view controller class is instantiated).
+    //Ensure that don't extist a view-controller and if exist that it's not destroyed
+    _getViewControllerInstance: function(routeObject) {
+        var self = this,
+            viewController = routeObject._viewController,
+            ViewControllerClass = routeObject._ViewController,
+            viewControllerOptions = routeObject._viewControllerOptions || {};
+
+        if (!viewController || viewController.isDestroyed === true) {
+            viewController = this.factory(ViewControllerClass, viewControllerOptions);
+            this.listenTo(viewController, 'destroy', this._removeViewController.bind(this, routeObject, viewController));
+        }
+        // this._viewControllers.push(viewController);
+
+        return viewController;
     },
     //Get default application view-controller class if no controller is specified
     getDefaultviewController: function() {
