87d2f68ed28c108a53d5f10e717d5c39041d4eba
Pedro Garrido <pedro.garrido@bq.com>
2015-05-05 13:01:21 +0200

    Unit Test Component
    Change-Id: Idf9ab93056f01d2984a1129052128f82c1e5c7a1

--- test/unit/jskeleton-src/component.spec.js
+++ test/unit/jskeleton-src/component.spec.js
@@ -4,43 +4,111 @@ describe('In Component module', function () {
 
     var sandbox = sinon.sandbox.create(),
         stubNewFactory,
-        modelComponent,
-        ViewController,
+        modelTest,
+        ModelComponent,
         ViewComponent,
-        LayoutComponent,
-        renderSpy;
-
+        ViewListComponent,
+        ViewControllerComponent,
+        MainApp,
+        testApp;
 
     beforeEach(function () {
 
-        modelComponent = new Backbone.Model({
-            title: 'Component HtmlBars'
+        ModelComponent = Backbone.Model.extend({
+            title: '',
+            author: ''
         });
 
-        ViewComponent = Backbone.Marionette.ItemView.extend({
-            template: '<strong> Test Title: </strong> <span class="title">{{title}}</span>',
-            model: modelComponent,
-            onRender: function(){
+        modelTest =new ModelComponent({
+            title: 'Title Component HtmlBars',
+            author: 'Author Component HtmlBars'
+        });
+
+        var templateComponent =
+            '<strong> Test Title: </strong> <span class="title">{{title}}</span>' +
+            '<strong> Test Author: </strong> <span class="author">{{author}}</span>' +
+            '<button class="buy-action"> Buy Book </button>' +
+            '<button class="return-action"> Return Book </button>' +
+            '<button class="back-action"> Come Back </button>';
+
+        ViewComponent = Jskeleton.ItemView.extend({
+            template: templateComponent,
+            model: modelTest,
+            ui: {
+                'alertContent': '.alert-content',
+                'buyButton': '.buy-action',
+                'returnButton': '.return-action',
+                'backButton': '.back-action'
+            },
+            events: {
+                'click @ui.buyButton': 'onActionBuy',
+                'click @ui.returnButton': 'onActionReturn',
+                'click @ui.backButton': 'onActionBack'
+            },
+            onRender: function () {},
+            onActionBuy: function () {},
+
+            onActionReturn: function () {
+                this.trigger('return');
+            },
+
+            onActionBack: function () {
+                this.channel.trigger('come:back');
             }
         });
-
-        ViewController = Jskeleton.ViewController.extend({
-            template: "{{@component name='ViewComponent'}}"
-        });
-
-
-        LayoutComponent = Backbone.Marionette.LayoutView.extend({
-          template: '<div class="content"></div>',
-          regions: {
-            content: ".content"
-          }
-        });
-
-        renderSpy = sinon.spy(ViewComponent.prototype, 'onRender');
 
         stubNewFactory = sandbox.stub(Jskeleton.factory, 'new', function () {
             return new ViewComponent();
         }).withArgs('ViewComponent');
+
+        ViewListComponent = ViewComponent.extend({
+            template: '<div class="book-list"><span>Book list:</span></div>',
+            onRender: function () {}
+        });
+
+        ViewControllerComponent = Jskeleton.ViewController.extend({
+            context: {
+                model: 'modelComponent'
+            },
+            events: {
+                'return @component.ViewComponent': 'onEventComponent'
+            },
+            onEventComponent: function () {}
+        });
+
+        var ChildApp = Jskeleton.ChildApplication.extend({
+            routes: {
+                '': {
+                    viewControllerClass: ViewControllerComponent,
+                    template: "{{@component name='ViewComponent' model=context.model}}"
+                },
+
+                'book/list': {
+                    viewControllerClass: ViewControllerComponent,
+                    template: "{{@component name='ViewListComponent'}}",
+                    eventListener: 'come:back'
+                }
+            }
+        });
+
+       var LayoutComponent = Jskeleton.LayoutView.extend({
+            regions: {
+                content: ".content"
+            }
+        });
+
+        MainApp = Jskeleton.Application.extend({
+            layout: {
+                layoutClass: LayoutComponent,
+                template: '<div class="content"></div>'
+            },
+            applications: {
+                'chilApp': {
+                    applicationClass: ChildApp,
+                    region: 'content'
+                }
+            }
+        });
 
     });
 
@@ -48,37 +116,59 @@ describe('In Component module', function () {
         sandbox.restore();
     });
 
+    describe('when define a new component,', function () {
+        var renderSpy,
+            eventComponentSpy,
+            eventViewControllerSpy,
+            eventChannelSpy;
 
-    describe('when define a new component', function () {
+        beforeEach(function () {
+            renderSpy = sinon.spy(ViewComponent.prototype, 'onRender');
+            eventComponentSpy = sinon.spy(ViewComponent.prototype, 'onActionBuy');
+            eventViewControllerSpy = sinon.spy(ViewControllerComponent.prototype, 'onEventComponent');
+            eventChannelSpy = sinon.spy(ViewListComponent.prototype, 'onRender');
 
-        var layout,
-            viewController;
-
-
-        beforeEach(function(){
-            layout = new LayoutComponent();
-            var mockApp = {
-                app: 'appMock',
-                channel: 'channelMock',
-                region: 'regionMock'
-            };
-
-            viewController = new ViewController(mockApp);
-
-            layout.render();
-            layout.content.show(viewController);
+            testApp = new MainApp();
+            testApp.start();
         });
 
-        it('and render this new component', function () {
+        afterEach(function () {
+            testApp.destroy();
+        });
+
+        it('it is rendered', function () {
             expect(renderSpy.calledOnce).to.be.equal(true);
         });
 
+        it('its model is rendered', function () {
+            expect(testApp.$rootEl.find('.title').length).to.be.above(0);
+            expect(testApp.$rootEl.find('.title')[0].innerHTML).to.be.equal(modelTest.get('title'));
 
-        it('and model is rendered', function () {
-            expect(layout.$el.find('.title').length).to.be.above(0);
-            expect(layout.$el.find('.title')[0].innerHTML).to.be.equal(modelComponent.get('title'));
+            expect(testApp.$rootEl.find('.author').length).to.be.above(0);
+            expect(testApp.$rootEl.find('.author')[0].innerHTML).to.be.equal(modelTest.get('author'));
         });
 
+        describe('it can throw events', function () {
+            var button;
+
+            it('that affects to own view', function () {
+                button = testApp.$rootEl.find('.buy-action');
+                button.click();
+                expect(eventComponentSpy.calledOnce).to.be.equal(true);
+            });
+
+            it('to its Controller View', function () {
+                button = testApp.$rootEl.find('.return-action');
+                button.click();
+                expect(eventViewControllerSpy.calledOnce).to.be.equal(true);
+            });
+
+            it.skip('using its own channel', function () {
+                button = testApp.$rootEl.find('.back-action');
+                button.click();
+                expect(eventChannelSpy.calledOnce).to.be.equal(true);
+            });
+        });
     });
 
 });
