657431e85f2428ab45c017a02a9868e2b6050272
Pedro Garrido <pedro.garrido@bq.com>
2015-05-11 13:12:30 +0200

    Adding Travis Runner
    Change-Id: Id3f03ae04a71d47b7ef19d1448301179fa50576b

---
+++ travis-runner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o pipefail
+
+echo "$TRAVIS_BRANCH"
+
+if [ "$TRAVIS_BRANCH" = "doc" ] && [ "$TRAVIS_PULL_REQUEST" != "true" ]
+then
+  echo "Deploying!" && \
+  cp CNAME _site
+  cd _site && \
+  git config --global user.email "travis@bq.com"
+  git config --global user.name "TRAVIS-CI"  
+  git init && \
+  git add . && \
+  git commit -m "deploy" && \
+  git push --force --quiet "https://${GH_OAUTH_TOKEN}@github.com/bq/jskeleton" doc:gh-pages > /dev/null 2>&1
+fi
