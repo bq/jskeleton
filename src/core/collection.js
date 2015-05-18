e4d274226037516b3ede6f314aeb17abef6eed0e
yonathan.rodriguez <yonathan.rodriguez@bq.com>
2015-05-18 16:33:46 +0200

    Added store collections support
    Change-Id: I1a6eaef5001d0a48397274b82baaa90368df1640

--- src/core/collection.js
+++ src/core/collection.js
@@ -5,4 +5,27 @@
 
 Jskeleton.Collection = Backbone.Collection.extend({
 
+    _prepareModel: function(attrs, options) {
+
+        if (this._isModel(attrs)) {
+            if (!attrs.collection) attrs.collection = this;
+            Jskeleton.modelStore.add(attrs);
+            return attrs;
+        }
+        options = options ? _.clone(options) : {};
+        options.collection = this;
+
+        var model = new this.model(attrs, options);
+        if (!model.validationError) {
+            // Add new model to modelStore or update it
+            Jskeleton.modelStore.add(model);
+            return model;
+        }
+        this.trigger('invalid', this, model.validationError, options);
+        return false;
+    },
+
+    _isModel: function(model) {
+        return model instanceof Backbone.Model;
+    }
 });
