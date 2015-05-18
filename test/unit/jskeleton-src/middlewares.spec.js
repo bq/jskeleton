4d04cf34d2401424f8cbc227aaf3e2896906374e
Jorge Serrano <jorge.serrano@bq.com>
2015-05-18 12:27:40 +0200

    middlewares unit tests
    Change-Id: Ie09006c657ba2a72edcdfb35525973b138bc69fd

---
+++ test/unit/jskeleton-src/middlewares.spec.js
@@ -0,0 +1,75 @@
+'use strict';
+
+describe('Application Middlewares', function(){
+    
+    describe('when run one middleware', function(){
+    
+        before(function(){
+            this.Application = Jskeleton.Application.extend({
+                middlewares : function(){
+                    this.calls.push(1);
+                },
+                calls : []
+             });
+
+            this.myApp = new this.Application();
+            
+        });
+
+        it('should be called middleware once',function(){
+            this.myApp._navigateTo('foo/bar',{},undefined);
+            expect(this.myApp.calls[0]).to.equal(1);
+        });
+    });
+    
+    describe('when run two  middlewares', function(){
+    
+        before(function(){
+            this.Application = Jskeleton.Application.extend({
+                middlewares : [function(){
+                    this.calls.push(1);
+                },function(){
+                    this.calls.push(2);
+                }],
+                calls : []
+             });
+
+            this.myApp = new this.Application();
+            
+        });
+
+        it('should be called middlewares twice',function(){
+            this.myApp._navigateTo('foo/bar',{},undefined);
+            expect(this.myApp.calls.length).to.equal(2);
+        });
+    });
+    
+    describe('when run three middlewares with params', function(){
+    
+        before(function(){
+            this.Application = Jskeleton.Application.extend({
+                middlewares : [function(){
+                    this.calls.push(1);
+                },function(){
+                    this.calls.push(2);
+                }, function(_routeParams){
+                    this.calls.push(_routeParams);
+                }],
+                calls : []
+             });
+
+            this.myApp = new this.Application();
+            
+        });
+
+        it('should be called middlewares three times',function(){
+            this.myApp._navigateTo('foo/bar',{},undefined);
+            expect(this.myApp.calls.length).to.equal(3);
+        });
+
+        it('should be called middlewares three times last one with params', function(){
+                        
+            expect(this.myApp.calls[2]).to.have.property('routeString');
+        });
+    });
+});
